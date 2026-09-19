cbb989e4f285d8bee8fb855343e0700c2088d2e7
Tarun Saratkar <tarunsaratkar@gmail.com>
2026-09-19 11:43:27 +0530

    feat(auth): connect request validation

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -3,10 +3,15 @@ import {
   signup,
   login,
 } from "../controllers/auth.controller.js";
+import validate from "../middleware/validation.middleware.js";
+import {
+  signupSchema,
+  loginSchema,
+} from "../validators/auth.validator.js";
 
 const router = express.Router();
 
-router.post("/signup", signup);
-router.post("/login", login);
+router.post("/signup", validate(signupSchema), signup);
+router.post("/login", validate(loginSchema), login);
 
 export default router;
